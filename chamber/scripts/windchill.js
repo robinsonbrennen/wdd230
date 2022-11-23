546250bc7b165c9ce2b17aabc9c91168d5d9d894
robinsonbrennen <rob16073@byui.edu>
2022-11-22 22:28:35 -0700

    weather api update and home website update

--- chamber/scripts/windchill.js
+++ chamber/scripts/windchill.js
@@ -1,13 +1,11 @@
 const currentTemp = document.querySelector("#temp");
-const windSpeed = document.querySelector("#speed");
-const chill = document.querySelector("#chill");
+const wSpeed = document.querySelector("#speed");
+const wChill = document.querySelector("#chill");
 const weatherIcon = document.querySelector('#icon');
-const captionDesc = document.querySelector('figcaption');
+const capDesc = document.querySelector('figcaption');
 
-// API url for Bozeman, MT with units in Fahrenheit (imperial) and my API ID
-const url = 'https://api.openweathermap.org/data/2.5/weather?q=Bozeman&units=imperial&limit=5&appid=e641a35ce91046feb0e7923b7d2101c0';
+const url = 'https://api.openweathermap.org/data/2.5/weather?lat=42.7249&lon=-110.9319&units=imperial&APPID=e641a35ce91046feb0e7923b7d2101c0';
 
-// Function to fetch API
 async function apiFetch() {
     try {
       const response = await fetch(url);
@@ -23,36 +21,27 @@ async function apiFetch() {
     }
 }
 
-// Function to get temperature and windspeed, and calculate windchill
 function windChill() {
-    //let currentTemp = 40;
-    //let windSpeed = 8;
-    //let windChill;
-    if (windSpeed > 3 && currentTemp <= 50) {
-        windChill = 35.74 + 0.6215 * currentTemp - 35.75 * windSpeed ** 0.16 + 0.4275 * currentTemp * windSpeed ** 0.16;
+    if (wSpeed > 3 && currentTemp <= 50) {
+        windChill = 35.74 + 0.6215 * currentTemp - 35.75 * wSpeed ** 0.16 + 0.4275 * currentTemp * wSpeed ** 0.16;
     } else {
         windChill = "NA";
     }
-    //document.getElementById("temp").innerHTML = currentTemp;
-    //document.getElementById("speed").innerHTML = windSpeed;
-    //document.getElementById("chill").innerHTML = windChill.toFixed(1);
     return windChill;
 }
 
-// Function to display data
 function displayResults(weatherData) {
-    // icon and current weather condition
-    const iconsrc = `https://openweathermap.org/img/w/${weatherData.weather[0].icon}.png`;
+    const iconSrc = `https://openweathermap.org/img/w/${weatherData.weather[0].icon}.png`;
     const desc = `${weatherData.weather[0].description}`;
-    icon.setAttribute('src', iconsrc);
+    icon.setAttribute('src', iconSrc);
     icon.setAttribute('alt', desc);
-    captionDesc.textContent = desc;
-    // temperature
+    capDesc.textContent = desc;
+
     currentTemp.innerHTML = `<strong>${weatherData.main.temp.toFixed(0)}</strong>`;
-    // wind speed
-    windSpeed.innerHTML = `${weatherData.wind.speed}`;
-    // wind chill
-    chill.innerHTML = windChill();
+
+    wSpeed.innerHTML = `${weatherData.wind.speed}`;
+
+    wChill.innerHTML = windChill();
 }
 
 apiFetch();
